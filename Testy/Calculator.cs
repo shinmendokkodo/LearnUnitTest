f467308fab46d023583fbad767066686e55d39d6
salee <salee@MIYAMOTO>
2022-10-04 10:48:51 -0700

    Add project files.

---
+++ Testy/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testy
+{
+    public class Calculator
+    {
+        // Adds two integers and returns an integer
+        public int AddNumber(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
